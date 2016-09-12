54c11cc6c9de0ff34bf9fafb4a42c33f527fcbce
Miguel Jacinto <miguel.jacinto@cleverti.com>
2016-09-12 14:34:14 +0100

    Add user entity

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace miguel\BacalhauBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use miguel\BacalhauBundle\Entity\User;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $user = new User();
+        $email = uniqid() . '@miguel.pt';
+        $user->setName('miguel')->setEmail($email);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
         return $this->render('miguelBacalhauBundle:Default:index.html.twig');
     }
 }
